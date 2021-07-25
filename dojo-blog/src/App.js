bf4007f5433a834772400ebacb682e3514ed4491
Aaditya Nitin <aadityanitin@gmail.com>
2021-07-25 19:57:58 +0530

    :star: feat: add navbar

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './navbar';
 
 function App() {
   const title = 'Welcome to the New Blog';
   const likes = 50;
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{ title }</h1>
         <p>Liked { likes } times</p>
