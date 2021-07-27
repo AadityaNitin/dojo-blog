615dc2917cc6c01c71f09862c85aef06babfa908
Aaditya Nitin <aadityanitin@gmail.com>
2021-07-27 15:48:10 +0530

    :star:feat:add pseudo blog

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
-    return (  
-        <div className="Home">
-            <h2>Homepage</h2>
-        </div>
-    );
-}
- 
+ const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+  return (
+    <div className="Home">
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{ blog.title }</h2>
+                <p>Wriiten by { blog.author }</p>
+            </div>
+        ))}
+    </div>
+  );
+};
+
 export default Home;
